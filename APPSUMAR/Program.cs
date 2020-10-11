0cf0e424f5ed161111febd91b479d2b04c1ea60e
Alex Garcia <gl0430032020@unab.edu.sv>
2020-10-11 16:25:23 -0500

    Suma
    app

---
+++ APPSUMAR/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace APPSUMAR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Bienvenido");
+
+
+            Console.WriteLine("Escribe tu nombre: ");
+            string nombre = Convert.ToString(Console.ReadLine());
+
+
+            Console.WriteLine("Escribe tu primer numero: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Escribe tu segundo numero: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+
+            double resultado;
+
+
+            resultado = num1 + num2;
+
+
+            Console.WriteLine("Tu resultado " + nombre + " es " + resultado);
+
+
+            Console.ReadLine();
+        }
+    }
+}
